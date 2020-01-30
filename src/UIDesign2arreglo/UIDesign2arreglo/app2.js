3df3e18e1517d411af8b0d3315be4b6b78ceeff7
Henry Aguilar <henrysagq@gmail.com>
2020-01-30 17:22:07 -0500

    Agregado Proyecto

---
+++ src/UIDesign2arreglo/UIDesign2arreglo/app2.js
@@ -0,0 +1,20 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyBRukFRkDbkipZQDlNASEbPJ5-fYD1JrQE",
+    authDomain: "proyecto-aal.firebaseapp.com",
+    projectId: "proyecto-aal"
+    ,storageBucket: 'gs://proyecto-aal.appspot.com'
+  });
+  
+  // var db = firebase.firestore();
+
+  // db.collection("users").add({
+  //   first: "Ada",
+  //   last: "Lovelace",
+  //   born: 1815
+  // })
+  // .then(function(docRef) {
+  //   console.log("Document written with ID: ", docRef.id);
+  // })
+  // .catch(function(error) {
+  //   console.error("Error adding document: ", error);
+  // });
